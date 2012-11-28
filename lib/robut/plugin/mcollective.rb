8e10e08a900b85f79b03338830539094f857d848
Jens Braeuer <jens.braeuer@commercetools.de>
2012-11-28 10:33:59 +0000

    Add helper methods.

--- lib/robut/plugin/mcollective.rb
+++ lib/robut/plugin/mcollective.rb
@@ -1,3 +1,34 @@
 module Robut::Plugin::Mcollective
+  def mc_status(r)
+    r[:body][:statuscode]
+  end
 
+  def mc_ok?(r)
+    mc_status(r) == 0
+  end
 end
+
+module Robut::Plugin::Mcollective::Packages
+  def packages_build_args(pkg)
+    { :packages => [{"name" => pkg, "version" => nil, "release" => nil}] }
+  end
+
+  def packages_status(r)
+    r[:body][:data]["status"]
+  end
+
+  def packages_packages(r)
+    r[:body][:data][:packages]
+  end
+
+  def packages_ok?(r)
+    packages_status(r) == 0
+  end
+end
+
+module Robut::Plugin::Mcollective::Service
+  def service_build_args(pkg)
+    { :service => pkg }
+  end
+end
+
